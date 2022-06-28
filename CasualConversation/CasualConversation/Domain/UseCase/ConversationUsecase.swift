2c0e6b32885f64b6e805430fa599eee012047787
YongwooKim <keepingitflow@gmail.com>
2022-06-28 19:54:05 +0900

    feat: ConversationUseCase  및 ConversationRepository 구조 생성
    기능 protocol 생성 및 TODO 정리

--- CasualConversation/CasualConversation/Domain/UseCase/ConversationUsecase.swift
+++ CasualConversation/CasualConversation/Domain/UseCase/ConversationUsecase.swift
@@ -1,8 +1,30 @@
 //
-//  ConversationUsecase.swift
+//  ConversationUseCase.swift
 //  CasualConversation
 //
-//  Created by Yongwoo Marco on 2022/06/23.
+//  Created by Yongwoo Marco on 2022/06/28.
 //
 
 import Foundation
+
+protocol ConversationRecodable {
+	// TODO: Conversation C 기능
+}
+
+protocol ConversationManagable {
+	// TODO: Conversation RUD 기능
+}
+
+final class ConversationUseCase: ConversationRecodable, ConversationManagable {
+	
+	struct Dependecy {
+		let repository: ConversationRepositoryProtocol
+	}
+	
+	let dependency: Dependecy
+	
+	init(dependency: Dependecy) {
+		self.dependency = dependency
+	}
+	
+}
