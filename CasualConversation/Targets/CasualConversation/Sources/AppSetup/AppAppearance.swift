2f06bad03cd7edcd2a9d77eed9e3a8e84f1fafb0
YongwooKim <keepingitflow@gmail.com>
2022-07-03 20:13:12 +0900

    chore: AppAppearance 및 AppConfigurations 구현
    주석으로 예제 표기

---
+++ CasualConversation/Targets/CasualConversation/Sources/AppSetup/AppAppearance.swift
@@ -0,0 +1,29 @@
+//
+//  AppAppearance.swift
+//  CasualConversation
+//
+//  Created by Yongwoo Marco on 2022/07/03.
+//  Copyright © 2022 pseapplications. All rights reserved.
+//
+
+import SwiftUI
+
+final class AppAppearance {
+	
+// MARK: - Example
+//	static func setupAppearance() {
+//		UINavigationBar.appearance().barTintColor = .black
+//		UINavigationBar.appearance().tintColor = .white
+//		UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+//	}
+	
+}
+
+// MARK: - Example
+//extension UINavigationController {
+//
+//	@objc override open var preferredStatusBarStyle: UIStatusBarStyle {
+//		return .lightContent
+//	}
+//
+//}
