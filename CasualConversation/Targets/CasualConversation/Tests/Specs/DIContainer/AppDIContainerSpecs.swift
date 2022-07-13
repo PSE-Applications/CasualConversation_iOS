b6733ea475a1add6474886f6958e6a6dfac9b71e
marco <keepingitflow@gmail.com>
2022-07-13 18:22:17 +0900

    test: App Layer UnitTest 작성
    AppDIContainer 메서드 테스트 작성

---
+++ CasualConversation/Targets/CasualConversation/Tests/Specs/DIContainer/AppDIContainerSpecs.swift
@@ -0,0 +1,49 @@
+//
+//  AppDIContainerSpecs.swift
+//  CasualConversation
+//
+//  Created by Yongwoo Marco on 2022/07/13.
+//  Copyright © 2022 pseapplications. All rights reserved.
+//
+
+@testable import CasualConversation
+
+import Quick
+import Nimble
+
+import Foundation
+
+final class AppDIContainerSpecs: QuickSpec {
+	
+	override func spec() {
+		
+		var appDIContainer: AppDIContainer!
+		
+		beforeEach {
+			appDIContainer = AppDIContainer()
+		}
+		
+		describe("Factory Methods") {
+			
+			context("call makePresentationDIContainer()") {
+				
+				it("return PresentationDIContainer instance") {
+					let presentationDIContainer = appDIContainer.makePresentationDIContainer()
+					
+					expect(presentationDIContainer).notTo(beNil())
+				}
+			}
+			
+			context("call PresentationEntryPoint()") {
+
+				it("return MainTabView instance as View") {
+					let mainTabView = appDIContainer.PresentationEntryPoint()
+
+					expect(mainTabView).notTo(beNil())
+				}
+			}
+		}
+		
+	}
+	
+}
