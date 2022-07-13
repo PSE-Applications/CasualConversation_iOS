7a34e33075c8952b77d0b1a0792a810214ade29e
marco <keepingitflow@gmail.com>
2022-07-14 01:04:11 +0900

    refactor: Quick 테스트방식 수정
    RecordRepositorySpecs 수정

--- CasualConversation/Targets/Data/Tests/Specs/RecordRepositorySpecs.swift
+++ CasualConversation/Targets/Data/Tests/Specs/RecordRepositorySpecs.swift
@@ -21,52 +21,63 @@ final class RecordRepositorySpecs: QuickSpec {
 		var fileManagerRepository: FileManagerRepository!
 		var recordRepository: RecordRepository!
 		
-		beforeSuite {
-			fileManagerRepository = FileManagerRepository()
-			recordRepository = RecordRepository(dependency: .init(
+		describe("as RecordRepository") {
+		
+			beforeSuite {
+				fileManagerRepository = FileManagerRepository()
+				recordRepository = RecordRepository(dependency: .init(
 					repository: fileManagerRepository
 				)
-			)
-		}
-		
-		afterSuite {
-			fileManagerRepository = nil
-			recordRepository = nil
-		}
-		
-		describe("Factory Methods") {
+				)
+			}
 			
-			context("call makeAudioRecorder()") {
-				it("return AVAudioRecorder instance as AudioRecorderProtocol") {
-					let recorder = recordRepository.makeAudioRecorder()
+			afterSuite {
+				fileManagerRepository = nil
+				recordRepository = nil
+			}
+			
+			
+			context("FactoryMethods") {
+				
+				it("call func makeAudioRecorder then AVAudioRecorder instance as AudioRecorderProtocol") {
+					// Arrange
+					var recorder: AVAudioRecorder
 					
+					// Act
+					recorder = recordRepository.makeAudioRecorder()!
+					
+					// Assert
 					expect(recorder).notTo(beNil())
 					expect(recorder).to(beAKindOf(AVAudioRecorder.self))
 				}
 			}
 			
 			// TODO: 임시 파일이 필요함 실제로 읽을 수 있는 filePath 필요
-//			context("call makeAudioPlayer(_:)") {
+//			it("call makeAudioPlayer(_:) then AVAudioPlayer as AudioPlayerProtocol") {
+//				// Arrange
 //				let filePath = URL(fileURLWithPath: "newFilePath")
+//				var player: AVAudioPlayer?
 //
-//				it("return AVAudioPlayer as AudioPlayerProtocol") {
-//					let player = recordRepository.makeAudioPlayer(from: filePath)
+//				// Act
+//				player = recordRepository.makeAudioPlayer(from: filePath) as? AVaudioPlayer
 //
-//					expect(player).notTo(beNil())
-//					expect(player).to(beAKindOf(AVAudioPlayer.self))
-//				}
+//
+//				// Assert
+//				expect(player).notTo(beNil())
+//				expect(player).to(beAKindOf(AVAudioPlayer.self))
 //			}
 			
-			context("call makeAudioPlayer(_:) by incorrect filePath") {
+			it("call func makeAudioPlayer by incorrect filePath then nil") {
+				// Arrange
 				let filePath = URL(fileURLWithPath: "failurePath")
+				var player: AVAudioPlayer?
 				
-				it("return nil") {
-					let player = recordRepository.makeAudioPlayer(from: filePath)
-					
-					expect(player).to(beNil())
-				}
+				// Act
+				player = recordRepository.makeAudioPlayer(from: filePath) as? AVAudioPlayer
+				
+				// Assert
+				expect(player).to(beNil())
 			}
 		}
 	}
-	
 }
