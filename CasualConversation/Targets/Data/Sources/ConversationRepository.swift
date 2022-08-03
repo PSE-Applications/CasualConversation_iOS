3795eaedc6e4830d605368ff46e1c264e6c132bd
marco <keepingitflow@gmail.com>
2022-08-03 17:34:48 +0900

    feat: ConversationRepository 구현
    - Conversation 타입 extension 추가 (데이터처리)
    - CRUD 동작 (fetch, create, update, delete) 구현

--- CasualConversation/Targets/Data/Sources/ConversationRepository.swift
+++ CasualConversation/Targets/Data/Sources/ConversationRepository.swift
@@ -11,9 +11,32 @@ import Domain
 
 import CoreData
 
-	public var fetchList: [Conversation] = [] // TODO: 처리 필요
+extension Conversation {
+	
+	init(entity: NSManagedObject) {
+		self.init(
+			id: entity.value(forKey: "id") as! UUID,
+			title: entity.value(forKey: "title") as? String,
+			topic: entity.value(forKey: "topic") as? String,
+			members: entity.value(forKey: "members") as! [String],
+			recordFilePath: entity.value(forKey: "recordFilePath") as! URL,
+			recordedDate: entity.value(forKey: "recordedDate") as! Date,
+			pins: entity.value(forKey: "pins") as! [TimeInterval]
+		)
+	}
+	
+	func setValues(_ entity: NSManagedObject) {
+		entity.setValue(id, forKey: "id")
+		entity.setValue(title, forKey: "title")
+		entity.setValue(topic, forKey: "topic")
+		entity.setValue(members, forKey: "members")
+		entity.setValue(recordFilePath, forKey: "recordFilePath")
+		entity.setValue(recordedDate, forKey: "recordedDate")
+		entity.setValue(pins, forKey: "pins")
+	}
 	
 }
+
 public struct ConversationRepository: Dependency {
 	
 	public struct Dependency {
@@ -36,19 +59,58 @@ public struct ConversationRepository: Dependency {
 extension ConversationRepository: ConversationRepositoryProtocol {
 	
 	public func fetchRequest() -> [Conversation]? {
+		let fetchRequest = ConversationEntity.fetchRequest()
+		let sortDescriptor = NSSortDescriptor.init(
+			key: #keyPath(ConversationEntity.recordedDate),
+			ascending: true
+		)
+		fetchRequest.sortDescriptors = [sortDescriptor]
+		do {
+			let list = try dependency.coreDataStack
+				.mainContext.fetch(fetchRequest)
+				.compactMap({ Conversation(entity: $0) })
+			return list
+		} catch let error {
+			debugPrint(CCError.persistenceFailed(reason: .coreDataUnloaded(error)))
+			return nil
+		}
 	}
+	
 	public func create(_ item: Conversation, completion: (CCError?) -> Void) {
-		// TODO: 처리 필요
+		let entity = ConversationEntity(context: dependency.coreDataStack.mainContext)
+		item.setValues(entity)
+		self.dependency.coreDataStack.saveContext()
+		completion(nil)
 	}
 	
 	public func update(after editedItem: Conversation, completion: (CCError?) -> Void) {
-		// TODO: 처리 필요
+		do {
+			let objects = try dependency.coreDataStack.mainContext.fetch(ConversationEntity.fetchRequest())
+			guard let object = objects.first(where: { $0.id == editedItem.id }) else {
+				completion(.persistenceFailed(reason: .coreDataUnloadedEntity))
+				return
+			}
+			editedItem.setValues(object)
+			self.dependency.coreDataStack.saveContext()
+			completion(nil)
+		} catch let error {
+			completion(.persistenceFailed(reason: .coreDataUnloaded(error)))
+		}
 	}
 	
 	public func delete(_ item: Conversation, completion: (CCError?) -> Void) {
-		// TODO: 처리 필요
+		do {
+			let objects = try dependency.coreDataStack.mainContext.fetch(ConversationEntity.fetchRequest())
+			guard let object = objects.first(where: { $0.id == item.id }) else {
+				completion(.persistenceFailed(reason: .coreDataUnloadedEntity))
+				return
+			}
+			self.dependency.coreDataStack.mainContext.delete(object)
+			self.dependency.coreDataStack.saveContext()
+			completion(nil)
+		} catch let error {
+			completion(.persistenceFailed(reason: .coreDataUnloaded(error)))
+		}
 	}
-	
-	public init() {}
 	
 }
