575f3627b7184550706cecaf2efa3b2ad2e121ad
marco <keepingitflow@gmail.com>
2022-08-03 17:32:26 +0900

    feat: .xcdatamodeld 생성 및 NSManagedObject 생성
    * CasualConversation.xcdatamodeld 파일
       - Entities 추가
    * NSMangedObject 생성 
       - Codegen - Manual/None 설정

---
+++ CasualConversation/Targets/Data/Sources/LocalRepository/CoreData/Entity/NoteEntity+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  NoteEntity+CoreDataClass.swift
+//  Data
+//
+//  Created by Yongwoo Marco on 2022/07/29.
+//  Copyright © 2022 pseapplications. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(NoteEntity)
+public class NoteEntity: NSManagedObject {
+
+}
