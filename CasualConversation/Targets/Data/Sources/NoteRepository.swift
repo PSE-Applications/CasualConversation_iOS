bb95d703e6c080998fe3f615290c5fd8acd9d555
marco <keepingitflow@gmail.com>
2022-08-03 21:07:11 +0900

    feat: NoteRepository CRUD 구현
    - fetch, create, delete, update 메서드 구현

--- CasualConversation/Targets/Data/Sources/NoteRepository.swift
+++ CasualConversation/Targets/Data/Sources/NoteRepository.swift
@@ -11,6 +11,29 @@ import Domain
 
 import CoreData
 
+extension Note {
+	
+	init(entity: NSManagedObject) {
+		self.init(
+			id: entity.value(forKey: "id") as! Identifier,
+			original: entity.value(forKey: "original") as! String,
+			translation: entity.value(forKey: "translation") as! String,
+			category: Category(rawValue: entity.value(forKey: "category") as! String)!,
+			references: entity.value(forKey: "references") as! [Identifier],
+			createdDate: entity.value(forKey: "createdDate") as! Date
+		)
+	}
+	
+	func setValues(_ entity: NSManagedObject) {
+		entity.setValue(id, forKey: "id")
+		entity.setValue(original, forKey: "original")
+		entity.setValue(translation, forKey: "translation")
+		entity.setValue(category.rawValue, forKey: "category")
+		entity.setValue(references, forKey: "references")
+		entity.setValue(createdDate, forKey: "createdDate")
+	}
+}
+
 public struct NoteRepository: Dependency {
 	
 	static let entityName = "NoteEntity"
@@ -31,22 +54,65 @@ public struct NoteRepository: Dependency {
 	
 }
 
+// MARK: - Usa CoreDataRepository
 extension NoteRepository: NoteRepositoryProtocol {
 	
 	public func fetch(filter item: Conversation? = nil) -> [Note]? {
-		[] // TODO: 처리 필요
+		let fetchRequest = NoteEntity.fetchRequest()
+		let sortDescriptor = NSSortDescriptor.init(
+			key: #keyPath(NoteEntity.createdDate),
+			ascending: true
+		)
+		fetchRequest.sortDescriptors = [sortDescriptor]
+		do {
+			let list = try dependency.coreDataStack
+				.mainContext.fetch(fetchRequest)
+				.compactMap({ Note(entity: $0) })
+			guard let itemID = item?.id else {
+				return list
+			}
+			return list.filter({ $0.references.contains(itemID) })
+		} catch let error {
+			debugPrint(CCError.persistenceFailed(reason: .coreDataUnloaded(error)))
+			return nil
+		}
 	}
 	
 	public func create(_ item: Note, completion: (CCError?) -> Void) {
-		// TODO: 처리 필요
+		let entity = NoteEntity(context: dependency.coreDataStack.mainContext)
+		item.setValues(entity)
+		self.dependency.coreDataStack.saveContext()
+		completion(nil)
 	}
 	
 	public func update(after editedItem: Note, completion: (CCError?) -> Void) {
-		// TODO: 처리 필요
+		do {
+			let objects = try dependency.coreDataStack.mainContext.fetch(NoteEntity.fetchRequest())
+			guard let object = objects.first(where: { $0.id == editedItem.id }) else {
+				completion(.persistenceFailed(reason: .coreDataUnloadedEntity))
+				return
+			}
+			editedItem.setValues(object)
+			self.dependency.coreDataStack.saveContext()
+			completion(nil)
+		} catch let error {
+			completion(.persistenceFailed(reason: .coreDataUnloaded(error)))
+		}
 	}
 	
 	public func delete(_ item: Note, completion: (CCError?) -> Void) {
-		// TODO: 처리 필요
+		do {
+			let objects = try dependency.coreDataStack.mainContext.fetch(NoteEntity.fetchRequest())
+			guard let object = objects.first(where: { $0.id == item.id }) else {
+				completion(.persistenceFailed(reason: .coreDataUnloadedEntity))
+				return
+			}
+			self.dependency.coreDataStack.mainContext.delete(object)
+			self.dependency.coreDataStack.saveContext()
+			completion(nil)
+		} catch let error {
+			completion(.persistenceFailed(reason: .coreDataUnloaded(error)))
+		}
 	}
 	
 }
