120ee3bfcfb9b026979313cf6b1fd349de4f418a
marco <keepingitflow@gmail.com>
2022-09-10 22:28:17 +0900

    refactor: FileManager 구성 수정
    - Documents 폴더 URL 프로퍼티 추가
    - Records 폴더 URL 프로퍼티 추가 및 디렉토리 생성 구현
    - baseDirectory 변경 -> Records 폴더
    - fileManager를 이용해서 녹음 파일에 접근

--- CasualConversation/Targets/Data/Sources/LocalRepository/FileManager/FileManagerRepository.swift
+++ CasualConversation/Targets/Data/Sources/LocalRepository/FileManager/FileManagerRepository.swift
@@ -5,6 +5,8 @@
 //  Created by Yongwoo Marco on 2022/06/23.
 //
 
+import Common
+
 import Foundation
 
 public protocol FileManagerRepositoryProtocol {
@@ -15,19 +17,36 @@ public protocol FileManagerRepositoryProtocol {
 public struct FileManagerRepository {
 	
 	private var fileManager = FileManager.default
+	private let documentsURL: URL
+	private let recordsDirectoryURL: URL
 	
-	public init() { }
+	public init() {
+		self.documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+		self.recordsDirectoryURL = documentsURL.appendingPathComponent("Records")
+		
+		do {
+			try fileManager.createDirectory(
+				at: self.recordsDirectoryURL,
+				withIntermediateDirectories: true,
+				attributes: nil)
+		} catch {
+			print("\(Self.self) \(#function) - 디렉토리 생성 실패")
+		}
+	}
 	
 }
 
 extension FileManagerRepository: FileManagerRepositoryProtocol {
 	
 	public var baseDirectory: URL {
-		fileManager.temporaryDirectory
+		self.recordsDirectoryURL
+	}
+
+	public func contents(atPath: String) -> Data? { // TODO: 매개변수 File 명으로 변경
+		let fileName = atPath.components(separatedBy: "/").last!
+		let path = self.recordsDirectoryURL.appendingPathComponent(fileName)
+		return fileManager.contents(atPath: path.path)
 	}
 	
-	public func contents(atPath: String) -> Data? {
-		return fileManager.contents(atPath: atPath)
-	}
 	
 }
